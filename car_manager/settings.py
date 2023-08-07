171da21cecf7a5ba0526ab512a90a787f4f6e944
Nikolay-Grozev <grozev27@gmail.com>
2023-08-07 07:44:51 +0300

    auto e-mail send

--- car_manager/settings.py
+++ car_manager/settings.py
@@ -1,8 +1,11 @@
 import os.path
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 from django.urls import reverse_lazy
 from celery.schedules import crontab
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'grozev27@gmail.com'
-EMAIL_HOST_PASSWORD = 'sorn hsdu tqsw suye'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
